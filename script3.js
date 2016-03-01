a5b96ab800548aec443d94f5d9ed735e8b477d37
Ozan Ocak <ozanocak@gmail.com>
2016-02-29 21:11:05 -0500

    first commit

---
+++ script3.js
@@ -0,0 +1,14 @@
+var myApp3=angular
+                 .module("myApp3",[])
+				 .controller("myFilterController",function($scope){
+					 var employees=[
+			            {name:"Ben",dateOfBirth: new Date("November 17,1986"),gender:"male",salary:"55000"},
+			            {name:"ozan",dateOfBirth: new Date("October 05,1985"),gender:"male",salary:"65000"},
+			            {name:"quan",dateOfBirth: new Date("February 13,1987"),gender:"female",salary:"35000"},
+			            {name:"orhan",dateOfBirth: new Date("September 02,1995"),gender:"male",salary:"15000"},
+			            {name:"sarah",dateOfBirth: new Date("May 15,1979"),gender:"female",salary:"50000"}
+			         ]; 
+					 $scope.employees=employees;
+					 $scope.rowlimit=3;
+				 });
+            
